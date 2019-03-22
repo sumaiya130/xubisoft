e6bb3e466893493183393ca36724f5ee58ae8f6c
sumaiya130 <48428248+sumaiya130@users.noreply.github.com>
2019-03-22 12:41:54 +0600

    Add files via upload

---
+++ xubisoft/js/main.js
@@ -0,0 +1,86 @@
+$(document).ready(function($){
+	"use strict";
+
+	// WOW Js Active
+	new WOW().init();
+
+	// ---- Active
+    $(".owl").owlCarousel();
+      //owlCarousel active in team
+        $('.owl-carousel-team').owlCarousel({
+            loop:true,
+            margin:20,
+            nav:false,
+            items:4,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+        })
+         //owlCarousel active in testomonial
+        $('.owl-carousel-testomonial').owlCarousel({
+            loop:true,
+            margin:20,
+            nav:false,
+            items:1,
+            
+      
+        })
+    
+    // Owl Next Privew Change
+    //$( ".owl-prev").html('<i class="fa screenshort-arow fa-chevron-left"></i>');
+    //$( ".owl-next").html('<i class="fa screenshort-arow fa-chevron-right"></i>');
+	
+	   <!--===magnificPopup ===-->
+        
+         $('#youtube-video').magnificPopup({
+             type:'iframe',
+             
+             iframe: {
+                
+
+                  patterns: {
+                    youtube: {
+                      index: 'youtube.com/', 
+                      id: 'v=',
+
+                      src: 'http://www.youtube.com/embed/%id%?autoplay=1' 
+                    }
+                  
+
+                  },
+
+                  srcAction: 'iframe_src',
+                }
+         });
+        // skillber js 
+        $('.skillbar').skillBars({
+
+        });
+	 // counter js
+	
+	      $('.count-number').each(function () {
+		$(this).prop('Counter',0).animate({
+			Counter: $(this).text()
+		}, {
+			duration: 4000,
+			easing: 'swing',
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
+		});
+    //mixitup js 
+    var mixer = mixitup('.portfolio_contant');
+    //porfolio magnificPopup js
+    $('.image-link').magnificPopup({type:'image'});
+
+
+}(jQuery));
